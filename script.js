326763a221201cef3ae1dfd875168df8f8cba2fd
Admin <admin@Admins-MacBook-Pro.local>
2023-11-08 21:43:15 +0300

    #5

--- script.js
+++ script.js
@@ -110,3 +110,26 @@ function nbYear(p0, percent, aug, p) {
   }
 
 //------------------Task 3--------
+
+function findShort(s){
+    let arr = s.split(" ")
+    res = 0;
+    for(let i = 0; i<arr.length; i++){
+        if(arr[i].length > res){
+            res = arr[i].length
+        }
+    }
+    console.log(res)
+  }
+
+  findShort("Oh Myt thore to")
+
+  //---------------Task 4---
+
+//   Simple, given a string of words, return the length of the shortest word(s).
+// String will never be empty and you do not need to account for different data types.
+
+function findShort(s){
+    let array = s.split(" ").sort((a, b) => a.length - b.length);
+      return array[0].length
+    }
